ba42e0c44bfc4cdcafcd802fd85dd843f7d2b62a
王可凡 <zcabkwa@ucl.ac.uk>
2022-03-23 15:14:31 +0000

    Fix Bug, Add Comments

--- model/Decoder.py
+++ model/Decoder.py
@@ -13,7 +13,6 @@ class Attention(nn.Module):
         self.key = nn.Linear(self.atten_size, self.atten_size)
         self.value = nn.Linear(self.atten_size, self.atten_size)
         self.fc = nn.Linear(self.n_dim, self.n_dim)
-        assert self.atten_size * self.n_heads == self.n_dim, 'Incorrect Dimension.'
 
     def forward(self, query, key, value, mask):
 
@@ -23,13 +22,16 @@ class Attention(nn.Module):
         q = self.query(query.reshape(batch_size, query_words, self.n_heads, self.atten_size))
         k = self.key(key.reshape(batch_size, key_words, self.n_heads, self.atten_size))
         v = self.value(value.reshape(batch_size, value_words, self.n_heads, self.atten_size))
-
+        
+        # (batch_size, query_words, n_heads, atten_size), (batch_size, key_words, n_heads, atten_size) -> (batch_size, n_heads, query_words, key_words)
         energy = torch.einsum('bnha, bmha -> bhnm', [q, k])/np.sqrt(self.atten_size)
     
         if mask != None:
             energy = energy.masked_fill(mask==0, float('-1e20'))
 
         dist = torch.softmax(energy, dim=3)
+
+        # (batch_size, query_words, query_words, key_words), (batch_size, value_words, n_heads, atten_size) -> (batch_size, n_heads, query_words, atten_size)
         attention = torch.einsum('bhnm, bmha->bnha',[dist, v]).reshape(batch_size, query_words, self.n_dim)
         
         out = self.fc(attention)
@@ -41,11 +43,14 @@ class DecoderBlock(nn.Module):
         super(DecoderBlock, self).__init__()
         self.masked_attention = Attention(n_dim, n_heads)
         self.encoder_decoder_attention = Attention(n_dim, n_heads)
+
+        # Forward expansion
         self.fc = nn.Sequential(
             nn.Linear(n_dim, forward_expansion),
             nn.ReLU(),
             nn.Linear(forward_expansion, n_dim)
         )
+
         self.ln = [nn.LayerNorm(n_dim, n_dim, device=device) for _ in range(3)]
         self.dropout = nn.Dropout(dropout)
     
@@ -56,6 +61,7 @@ class DecoderBlock(nn.Module):
         out = self.dropout(self.ln[2](self.fc(snd) + snd))
 
         if return_extra:
+            # Only return extra in the last decoderblock
             return out, snd
         else:
             return out
@@ -67,6 +73,8 @@ class Decoder(nn.Module):
         self.embedding = embedding
         self.n_vocab = self.embedding.word_embeddings.num_embeddings
         self.n_dim = self.embedding.word_embeddings.embedding_dim
+
+        # n - 1 regular decoderblock and 1 extra decoderblock returns extra
         self.decoders = nn.ModuleList(
             [
                 DecoderBlock(self.n_dim, n_heads, forward_expansion, dropout, device) for _ in range(n_decoders - 1)
@@ -74,16 +82,18 @@ class Decoder(nn.Module):
         )
         self.extra_decoder = DecoderBlock(self.n_dim, n_heads, forward_expansion, dropout, device)
         self.fc_out = nn.Linear(self.n_dim, self.n_vocab)
-        self.dropout = nn.Dropout(dropout)
     
     def forward(self, input_ids, token_type_ids, key, value, src_mask, trg_mask, return_extra=False):
 
         x = self.embedding(input_ids, token_type_ids)
         for decoder in self.decoders:
             x = decoder(x, key, value, src_mask, trg_mask)
-        s_t, q_t = decoder(x, key, value, src_mask, trg_mask, True)
+        
+        # Return the last two terms
+        s_t, q_t = self.extra_decoder(x, key, value, src_mask, trg_mask, True)
         x = self.fc_out(s_t)
         if return_extra:
+            # Only return extra when using pointer
             return x, s_t, q_t
         else:
             return x
--- model/Encoder.py
+++ model/Encoder.py
@@ -5,11 +5,17 @@ class Encoder(nn.Module):
 
     def __init__(self, BertModel):
         super(Encoder, self).__init__()
+        
+        # Load Pretrained Bert Model
         self.BertModel = AutoModel.from_pretrained(BertModel)
 
     def save_to_disk(self, file_name='bert-base-uncased'):
+
+        # Save to disk to prevent repeated downloading
         self.BertModel.save_pretrained(file_name)
 
     def forward(self, input_ids, token_type_ids, attention_mask):
+
+        # Get the output of the Bert Model
         out = self.BertModel(input_ids, token_type_ids, attention_mask)
         return out
